db6f9288caf639f8fdd5dbf61feb9b389239a824
devsabbirhossain <sabbir2dev@gmail.com>
2024-08-31 03:09:49 +0600

    add weather components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import MistImage from "./assets/backgrounds/mist.jpeg";
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
   return (
     <>
-      <Header />
+      <div
+        style={{ backgroundImage: `url('${MistImage}')` }}
+        className="grid place-items-center h-screen bg-no-repeat bg-cover">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
